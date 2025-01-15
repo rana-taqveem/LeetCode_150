6249eabca9716a6badb3c08d5d1894357ac09d6e
rana-taqveem <ranataqveem@gmail.com>
2025-01-15 21:10:04 +0500

    add additional solutions

--- LeetCode_150/MergeSortedArray.cs
+++ LeetCode_150/MergeSortedArray.cs
@@ -12,8 +12,8 @@ namespace LeetCode_150
         {
 
             int[] result = new int[nums1.Length];
-            int num2_index = 0;
-            int num1_index = 0;
+            int y = 0;
+            int x = 0;
             int index = 0;
 
             if (m == 0 && n != 0)
@@ -26,57 +26,119 @@ namespace LeetCode_150
             {
                 while (index < m + n)
                 {
-                    if (num1_index < m && num2_index < n)
+                    if (x < m && y < n)
                     {
-                        if (nums2[num2_index] == nums1[num1_index])
+                        if (nums2[y] == nums1[x])
                         {
-                            result[index] = nums1[num1_index];
-                            result[index + 1] = nums2[num2_index];
-                            num1_index++; // 2
-                            num2_index++; // 1
+                            result[index] = nums1[x];
+                            result[index + 1] = nums2[y];
+                            x++; // 2
+                            y++; // 1
                             index = index + 2; //3
                         }
                     }
 
-                    if (num2_index < n)
+                    if (y < n && (x == m || (nums2[y] < nums1[x])))
                     {
-                        if (num1_index == m)
+                        if (x == m || nums2[y] < nums1[x])
                         {
-                            result[index] = nums2[num2_index];
-                            num2_index++; // 2, 3
+                            result[index] = nums2[y];
+                            y++; // 2, 3
                             index++; //5, 6
                         }
-                        else if (nums2[num2_index] < nums1[num1_index])
-                        {
-                            result[index] = nums2[num2_index];
-                            num2_index++;
-                            index++;
-                        }
-
                     }
 
-                    if (num1_index < m)
+                    if (x < m && (y == n || (nums1[x] < nums2[y])))
                     {
-                        if (num2_index == n)
-                        {
-                            result[index] = nums1[num1_index];
-                            num1_index++;
-                            index++;
-                        }
-                        else if (nums1[num1_index] < nums2[num2_index])
-                        {
-
-                            result[index] = nums1[num1_index];
-                            num1_index++; // 3
-                            index++;  // 4,
-                        }
-
+                        result[index] = nums1[x];
+                        x++; // 3
+                        index++;  // 4,
                     }
 
                 }
 
                 for (int i = 0; i < m + n; i++)
                     nums1[i] = result[i];
+            }
+        }
+
+
+
+    
+
+        public static void Merge_2(int[] nums1, int m, int[] nums2, int n)
+        {
+            int[] result = new int[nums1.Length];
+            int x = m - 1;
+            int y = n - 1;
+            int z = m + n - 1;
+
+            int index = 0;
+
+            if (m == 0 && n != 0)
+            {
+                for (int i = 0; i < n; i++)
+                    nums1[i] = nums2[i];
+            }
+
+            if (m != 0 && n != 0)
+            {
+                while (n > 0)
+                {
+                    if (x > -1 && nums1[x] > nums2[y])
+                    {
+                        nums1[z] = nums1[x];
+                        nums1[x] = 0;
+                        x = x - 1;
+                    }
+                    else
+                    {
+                        nums1[z] = nums2[y];
+                        y = y - 1;
+                        n = n - 1;
+                    }
+
+                    z--;
+
+                    index++;
+                }
+            }
+        }
+        public static void Merge_1(int[] nums1, int m, int[] nums2, int n)
+        {
+
+            int[] result = new int[nums1.Length];
+            int x = m-1;
+            int y = 0;
+            int index = 0;
+
+            if (m == 0 && n != 0)
+            {
+                for (int i = 0; i < n; i++)
+                    nums1[i] = nums2[i];
+            }
+            
+            if (m != 0 && n != 0)
+            {
+                while (n > 0)
+                {
+                    if (x > -1 && nums1[x] > nums2[y])
+                    {
+                        nums1[x + 1] = nums1[x];
+                        nums1[x] = 0;
+                        x = x - 1;
+                    }
+                    else
+                    {
+                        nums1[x + 1] = nums2[y];
+                        y = y + 1;
+                        m = m + 1;
+                        n = n - 1;
+                        x = m - 1;
+                    }
+
+                    index++;
+                }
             }
         }
     }
--- LeetCode_150/Program.cs
+++ LeetCode_150/Program.cs
@@ -7,11 +7,24 @@
             Console.WriteLine("Hello, World!");
 
 
-            int[] nums1 = [1, 2, 3, 0, 0, 0];
-            int[] nums2 = [2, 5, 6];
-            int m = 3, n = 3;
+            //int[] nums1 = [1, 2, 3, 0, 0, 0];
 
-            MergeSortedArray.Merge(nums1,m, nums2, n);
+            
+
+
+            //int[] nums2 = [2, 5, 6];
+
+
+
+            int[] nums1 = [2, 0, 0, 0];
+
+
+
+
+            int[] nums2 = [1, 1, 5];
+            int m = 1, n = 3;
+
+            MergeSortedArray.Merge_2(nums1,m, nums2, n);
         }
     }
 }
